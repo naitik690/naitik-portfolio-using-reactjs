ccb45b82e3cc9ac260ae22c0dc32436570a1e719
myuex <myuex.srg@gmail.com>
2025-05-26 20:59:54 +0530

    initial setup

---
+++ src/skill.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function Skills() {
+
+
+  return (
+    <>
+       <div className="skill">
+       <h2 className='yellow'>✨Technical Skills</h2>
+       <div className="li">
+<li>Frontend Technologies: HTML5, CSS3, JavaScript (ES6+), ReactJS</li>
+<li>Styling: CSS Flexbox, Grid, Responsive Design, Bootstrap </li>
+<li>JavaScript Frameworks & Libraries: ReactJS, JSX, React Router, State Management (useState, useEffect, etc.)</li>
+<li>Version Control: Git, GitHub  </li>
+</div>
+</div>
+    </>
+  )
+}
+
+export default Skills;
